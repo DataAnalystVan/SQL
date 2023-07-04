5714cb9b8d45d80a61f82cdb04a694c120bec1a2
DataAnalystVan <134701010+DataAnalystVan@users.noreply.github.com>
2023-07-04 11:23:50 +0800

    Add files via upload

---
+++ Having Clause - VNocilo.sql
@@ -0,0 +1,32 @@
+SELECT JobTitle, COUNT(JobTitle)
+FROM SQLTutorial.dbo.EmployeeDemographics
+JOIN SQLTutorial.dbo.EmployeeSalary
+     ON EmployeeDemographics.EmployeeID = EmployeeSalary.EmployeeID
+GROUP BY JobTitle 
+
+
+SELECT JobTitle, AVG(Salary)
+FROM SQLTutorial.dbo.EmployeeDemographics
+JOIN SQLTutorial.dbo.EmployeeSalary
+     ON EmployeeDemographics.EmployeeID = EmployeeSalary.EmployeeID
+GROUP BY JobTitle 
+HAVING AVG(Salary) > 45000
+ORDER BY AVG(Salary)
+
+
+
+
+
+
+
+
+
+
+DELETE FROM SQLTutorial.dbo.EmployeeSalary 
+WHERE EmployeeID = 1005
+
+
+
+
+
+
