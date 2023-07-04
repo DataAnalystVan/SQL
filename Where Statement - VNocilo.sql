f913d3fc3d890c7c99af7ba529205414135f8627
DataAnalystVan <134701010+DataAnalystVan@users.noreply.github.com>
2023-07-04 11:20:47 +0800

    Add files via upload

---
+++ Where Statement - VNocilo.sql
@@ -0,0 +1,9 @@
+SELECT *
+FROM EmployeeDemographics
+ORDER BY 4 DESC, 5 DESC
+
+--SELECT(Gender), COUNT(Gender) AS CountGender
+--FROM EmployeeDemographics
+--WHERE Age > 31
+--GROUP BY Gender
+--ORDER BY Gender 
